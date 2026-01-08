65ec355516c4b364d6bf9dfca34a28f7e0c34b05
Cláudio António Rumal <73038881+clossvany@users.noreply.github.com>
2026-01-08 20:50:39 +0000

    Update day2-building-a-calculator.py

--- day2-building-a-calculator.py
+++ day2-building-a-calculator.py
@@ -8,3 +8,13 @@ total_bill = bill + total_tip
 bill_per_person = total_bill / people
 final_total = round(bill_per_person, 2)
 print(f"Total to pay each person is: {final_total}")
+
+#just another day run :D3
+
+print("Welcome to Rollercoaster")
+hight = int(input("What is your hight? "))
+
+if hight >= 120:
+    print("You can ride the Rollercoaster.")
+else:
+    print("Sorry! You have to grow taller before you can ride, maybe next year.")
